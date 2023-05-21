daddccf51d8656fbe01b288a515970fbc2eb0fac
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-05-21 21:02:47 +0200

    db_update

--- session.py
+++ session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root:prodigy1#@localhost:3306/dvd_store")
+engine = create_engine("mysql+pymysql://root:password#@localhost:3306/dvd_store")
 Session = sessionmaker(engine)
 session = Session()
