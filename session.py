c2b9976eae7b100f965a4ca00508a3ac17e7d9d1
MariuszOwczarek <89746880+MariuszOwczarek@users.noreply.github.com>
2023-05-21 20:25:22 +0200

    dvd_store models

---
+++ session.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("mysql+pymysql://root:prodigy1#@localhost:3306/dvd_store")
+Session = sessionmaker(bind=engine)
+session = Session()
+
